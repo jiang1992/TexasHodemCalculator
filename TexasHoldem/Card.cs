365105ee3a54c20deab4f4a108400c853e2d083b
jiang <jiang46@purdue.edu>
2015-06-16 01:03:11 -0400

    TBH。。。

---
+++ TexasHoldem/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem
+{
+    class Card
+    {
+
+        int color;
+        int number; 
+        public Card(int color, int number) {
+            this.color = color;
+            this.number = number;         
+        }
+        public int getColor() { 
+            
+            if(this.color == 0){
+
+                Console.WriteLine("Clubs");
+            }
+            else if (this.color == 1)
+            {
+                Console.WriteLine("Spades");
+            }
+            else if (this.color == 2)
+            {
+                Console.WriteLine("Diamonds");
+            }
+            else if (this.color == 3)
+            {
+                Console.WriteLine("Hearts");
+            }
+            else { }
+            return this.color;
+        }
+
+        public int getNumber()
+        {            
+            return this.number;
+        }
+    }
+}
---
+++ TexasHoldem/TheDeck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem
+{
+    class TheDeck
+    {
+       
+        public static int numberOfColor = 4;
+        public static int numberOfNumber = 13;
+        public static int deckSize = numberOfColor * numberOfNumber;
+        public static Card[] RemainingDeck = new Card[deckSize];
+        public TheDeck() {
+
+            newDeck();
+        }
+
+        private void newDeck() {
+            int k = 0;
+            for (int i = 0; i < numberOfColor; i++) 
+            {
+
+                for (int j = 1; j < numberOfNumber+1; j++ )
+                {
+                    RemainingDeck[k] = new Card(i,j);
+                    k++;
+                }
+
+            }           
+        }// this creates a new deck
+    }
+}
